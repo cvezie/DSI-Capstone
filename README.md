55e773b79c45fc17eac17182310dcdf758f1c157
Claire Vezie <clairevezie@Claires-MacBook-Pro.local>
2017-12-04 12:26:38 -0800

    capstone work in progress

---
+++ README.md
@@ -0,0 +1,1 @@
+Data from 2007-2016 unless stated as 2017 in filename.
---
+++ __init__.py
@@ -0,0 +1,13 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import uuid
+
+
+from scipy import stats
+from sklearn.decomposition import PCA
+from sklearn.model_selection import train_test_split
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import StandardScaler, MinMaxScaler
+
